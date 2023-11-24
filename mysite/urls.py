55471f1c214c7f7552b38c8166671ba6dc4a5c15
Šimon Murín <simonko.murin@gmail.com>
2023-11-24 11:59:20 +0100

    pridanie skola a studentov

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', include('hello.urls')),
     path('skuska/', include('skuska.urls')),
+    path('skola/', include('skola.urls')),
 ]
--- skola/views.py
+++ skola/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from . models import *
 
-# Create your views here.
+def vypis_student():
+    studenti = Student.objects.all()
+    return HttpResponse(studenti)
