c6af28294aed98d7708721fd4cfb6845b325b10f
Šimon Murín <simonko.murin@gmail.com>
2024-02-02 11:36:27 +0100

    shop produkty prekliky

--- shop/urls.py
+++ shop/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('produkty/', views.vypis_produkty, name='vypis-produkty'),
     path('uzivatelia/', views.vypis_uzivatelov, name='vypis-uzivatelov'),
     path('objednavky/', views.vypis_objednavky, name='vypis-objednavky'),
+    path('kategorie/<kategoria>/', views.vypis_kategoriu, name='kategoria'),
+    path('produkty/<produkt>/', views.vypis_produkt, name='produkt'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -22,3 +22,16 @@ def vypis_uzivatelov(request):
 def vypis_objednavky(request):
     objednavky = Objednavka.objects.all().order_by("id_objednavky")
     return render(request, "shop/index.html", {"objednavky":objednavky})
+
+def vypis_kategoriu(request, kategoria):
+    kategoria = Kategoria.objects.get(nazov=kategoria)
+    produkty = Produkt.objects.filter(kategoria_id=kategoria.pk).order_by("nazov")
+    produkt_list = []
+    for produkt in produkty:
+        produkt_list.append(produkt)
+    #return HttpResponse(f"{trieda}<br>{ucitel}<br>{studenti_list}")
+    return render(request, "shop/kategoria.html", {"produkty":produkt_list, "kategoria":kategoria})
+
+def vypis_produkt(request, produkt):
+    produkt = Produkt.objects.get(product_id=produkt)
+    return render(request, "shop/produkt.html", {"produkt":produkt})
